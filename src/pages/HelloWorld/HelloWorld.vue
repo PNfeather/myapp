4d74636eb4f2c5e928112eaee880788c44bfab23
PNFeather <921192114@qq.com>
2018-09-18 14:52:15 +0800

    提交

--- src/pages/HelloWorld/HelloWorld.vue
+++ src/pages/HelloWorld/HelloWorld.vue
@@ -1,12 +1,10 @@
 <template>
   <div class="firstPage" @click="jumpTest" ref="test">
     {{ msg }}
-    <co-longTouch @callBack="longTouch"></co-longTouch>
   </div>
 </template>
 
 <script>
-import longTouch from '@/components/longTouch/longTouch.vue'
 export default {
   name: 'firstPage',
   data () {
@@ -25,7 +23,6 @@ export default {
     }
   },
   components: {
-    'co-longTouch': longTouch
   }
 }
 </script>

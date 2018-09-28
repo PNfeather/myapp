87eb460e11c276519d23dc82ce0389ce2fb179ae
PNfeather <921192114@qq.com>
2018-09-28 11:28:53 +0800

    添加mintui

--- src/main.js
+++ src/main.js
@@ -6,6 +6,37 @@ import router from './router';
 import FastClick from 'fastclick';
 import http from '@/plugins/http';
 import '@/plugins/longTouch';
+import {
+  Header, Button, Popup, Swipe, SwipeItem, Tabbar, TabItem, MessageBox, Toast, InfiniteScroll, Spinner, Loadmore, Picker, Indicator, DatetimePicker, TabContainer, TabContainerItem, CellSwipe, Switch, Lazyload
+} from 'mint-ui';
+
+// mint-ui组件引入
+Vue.component(Header.name, Header);
+Vue.component(Button.name, Button);
+Vue.component(Popup.name, Popup);
+Vue.component(Swipe.name, Swipe);
+Vue.component(SwipeItem.name, SwipeItem);
+Vue.component(Tabbar.name, Tabbar);
+Vue.component(TabItem.name, TabItem);
+Vue.component(Spinner.name, Spinner);
+Vue.component(Loadmore.name, Loadmore);
+Vue.component(Picker.name, Picker);
+Vue.component(DatetimePicker.name, DatetimePicker);
+Vue.component(CellSwipe.name, CellSwipe);
+Vue.component(TabContainer.name, TabContainer);
+Vue.component(TabContainerItem.name, TabContainerItem);
+Vue.component(Switch.name, Switch);
+
+Vue.use(InfiniteScroll);
+Vue.use(Lazyload, {
+  loading: require('./assets/image/defaultImage.png'),
+  attempt: 3
+});
+
+Vue.$messagebox = Vue.prototype.$messagebox = MessageBox;
+Vue.$toast = Vue.prototype.$toast = Toast;
+Vue.$indicator = Vue.prototype.$indicator = Indicator;
+Vue.$swipe = Vue.prototype.$swipe = Swipe;
 
 Vue.config.productionTip = false;
 
--- src/plugins/http/index.js
+++ src/plugins/http/index.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
-console.log(axios);
-
 export default {
   install (Vue) {
     Vue.prototype.$http = axios;

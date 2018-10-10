1f354626538b66ee29eb9dbd2fdd2f82d64b2a02
PNfeather <921192114@qq.com>
2018-10-10 15:38:37 +0800

    提交

--- src/plugins/findScroll/index.js
+++ src/plugins/findScroll/index.js
@@ -4,12 +4,12 @@ import {getCss} from '@/tools/common';
 Vue.directive('findScroll', {
   inserted: function (el, binding) { // 获取当前元素第一个能滚动的父元素的指令，并以当前元素及找到的父元素为参数执行表达式
     let targetEl = el;
-    (function goTop (scrollEl) {
-      if (scrollEl) {
-        if (getCss(scrollEl, 'overflow') === 'auto' || getCss(el, 'overflow') === 'scroll') {
-          binding.value(targetEl, scrollEl);
+    (function getScroll (element) {
+      if (element.nodeName !== 'BODY') {
+        if (getCss(element, 'overflow') === 'auto' || getCss(el, 'overflow') === 'scroll') {
+          binding.value(targetEl, element);
         } else {
-          goTop(scrollEl.parentNode);
+          getScroll(element.parentNode);
         }
       } else {
         return false;

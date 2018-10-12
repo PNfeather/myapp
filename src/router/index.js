1c2ee5bac0a92c62932b0f2ad247fd84fa33f383
PNfeather <921192114@qq.com>
2018-10-12 16:12:23 +0800

    提交

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,6 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import routeConfig from './routeConfig';
 import IndexPage from '@/pages/index/index.vue';
-// import _ from '@/plugins/lodash';
 
 Vue.use(Router);
 

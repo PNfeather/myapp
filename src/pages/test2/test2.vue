61b977620be795dae32cbf2e4d681d998e501676
PNfeather <921192114@qq.com>
2018-11-23 10:23:09 +0800

    提交

--- src/pages/test2/test2.vue
+++ src/pages/test2/test2.vue
@@ -14,7 +14,7 @@ export default {
   name: 'test2',
   data () {
     return {
-      msg: 'test2Page',
+      msg: 'goToPage3',
       imgList: [
         'https://imgsa.baidu.com/exp/w=480/sign=ccfcf0f28c35e5dd902ca4d746c7a7f5/b03533fa828ba61e2bedd88f4834970a304e590a.jpg',
         'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1542718652331&di=3d61ab43d0f2afc10a52d4f851ab46d3&imgtype=0&src=http%3A%2F%2Fimg1.cache.netease.com%2Fcatchpic%2FC%2FC2%2FC212F0DF5C3067A73362C5F7D496E19D.jpg',

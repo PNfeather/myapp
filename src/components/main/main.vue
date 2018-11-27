72ebc621ac7e519c5a17709a97e2c697b82ef80e
PNfeather <921192114@qq.com>
2018-11-27 16:47:45 +0800

    提交

---
+++ src/components/main/main.vue
@@ -0,0 +1,40 @@
+<template>
+  <main class="main" ref="main">
+    <slot></slot>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'app-main',
+  props: {
+    key: {
+      type: String,
+      default: ''
+    }
+  },
+  data () {
+    return {
+    };
+  },
+  methods: {
+
+  },
+  components: {},
+  created () {},
+  mounted () {
+    this.$nextTick(() => {
+
+    });
+  }
+};
+</script>
+
+<style lang='less' scoped>
+  .main{
+    position: absolute;
+    top: 0;right: 0;
+    bottom: 0;left: 0;
+    overflow: hidden;
+  }
+</style>
---
+++ src/pages/directTestPage/directTestPage.vue
@@ -0,0 +1,31 @@
+<template>
+    <app-page>
+        <app-header :title="title"></app-header>
+        <main></main>
+    </app-page>
+</template>
+
+<script>
+    export default {
+      name: 'direct-test-page',
+      props: {
+        key: {
+          type: String,
+          default: ''
+        }
+      },
+      data () {
+        return {
+            title: '指令测试页面'
+        };
+      },
+      methods: {},
+      components: {},
+      created () {},
+      mounted () {}
+    };
+</script>
+
+<style lang='less' scoped>
+
+</style>

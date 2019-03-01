9f3a6c351d741129d489655138518d3fbcc6ccf6
PNfeather <921192114@qq.com>
2019-03-01 16:25:00 +0800

    提交

---
+++ src/pages/index/data/index.js
@@ -0,0 +1,24 @@
+export const indexJumpConfig = [
+  {
+    text: '点击进入mint-ui轮播',
+    path: 'mintUi'
+  }, {
+    text: '点击进入模拟考试',
+    path: 'exam'
+  }, {
+    text: '点击进入模拟分页加载数据页',
+    path: 'scrollModel'
+  }, {
+    text: '点击进入图表展示页',
+    path: 'multipleCharts'
+  }, {
+    text: '点击进入时间组件合图表页',
+    path: 'echartsModel'
+  }, {
+    text: '点击进入地图示例页',
+    path: 'mapModel'
+  }, {
+    text: '点击进入上传图片示例页',
+    path: 'uploadImgModel'
+  }
+];
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -36,6 +36,7 @@
   // import {mapState} from 'vuex';
   import storageStore from '@/tools/localStorage';
   import _ from '@/plugins/lodash';
+  import {indexJumpConfig} from './data';
   export default {
     name: 'firstPage',
     data () {
@@ -45,30 +46,7 @@
         showJumpPopup: false,
         jumpPopupList: _.cloneDeep(rightBtnConfig),
         justForMe: false,
-        indexConfig: [
-          {
-            text: '点击进入mint-ui轮播',
-            path: 'mintUi'
-          }, {
-            text: '点击进入模拟考试',
-            path: 'exam'
-          }, {
-            text: '点击进入模拟分页加载数据页',
-            path: 'scrollModel'
-          }, {
-            text: '点击进入图表展示页',
-            path: 'multipleCharts'
-          }, {
-            text: '点击进入时间组件合图表页',
-            path: 'echartsModel'
-          }, {
-            text: '点击进入地图示例页',
-            path: 'mapModel'
-          }, {
-            text: '点击进入上传图片示例页',
-            path: 'uploadImgModel'
-          }
-        ]
+        indexConfig: [...indexJumpConfig]
       };
     },
     created () {

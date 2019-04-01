bb553dc1a10b0719ff5fdd4b0071153a489bf4e6
PNFeather <921192114@qq.com>
2019-04-01 14:37:38 +0800

    提交

--- src/pages/testDom/testDom.vue
+++ src/pages/testDom/testDom.vue
@@ -1,17 +1,10 @@
 <template>
     <app-page id="test-dom">
       <app-header :title="title"></app-header>
-      <!--<popupOne></popupOne>-->
-      <!--<popupTwo></popupTwo>-->
-      <!--<popupThree></popupThree>-->
     </app-page>
 </template>
 
 <script>
-  import {mapMutations} from 'vuex';
-  import popupOne from './compunents/popupOne';
-  import popupTwo from './compunents/popupTwo';
-  import popupThree from './compunents/popupThree';
   import formatNum from './common/formatNum';
     export default {
       name: 'test-dom',
@@ -21,7 +14,6 @@
         };
       },
       methods: {
-        ...mapMutations(['clearTestPagePopup']),
         test () { // 测试重复调接口会拦截
           this.$http.get('/test').then((res) => {
             console.log(res);
@@ -65,14 +57,7 @@
           return arr;
         }
       },
-      beforeRouteLeave (to, from, next) {
-        this.clearTestPagePopup();
-        next();
-      },
       components: {
-        popupOne,
-        popupTwo,
-        popupThree
       },
       created () {
         // this.test();

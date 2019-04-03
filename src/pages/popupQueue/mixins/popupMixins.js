c199149e0d889c6bb1666c9647736e919040388c
PNFeather <921192114@qq.com>
2019-04-03 11:49:58 +0800

    提交

--- src/pages/popupQueue/mixins/popupMixins.js
+++ src/pages/popupQueue/mixins/popupMixins.js
@@ -5,13 +5,14 @@ import {needPopupQueuePage} from './needPopupQueuePage';
 const popupMixins = {
   data () {
     return {
+      isPopupQueuePage: needPopupQueuePage.includes(this.$route.path),
       popupQueueToggleNameArray: []
     };
   },
   methods: {
     ...mapMutations(['pushPagePopupQueueArray', 'shiftPagePopupQueueArray']),
     commonPushPopupQueueArray (toggleName) {
-      if (needPopupQueuePage.includes(this.$route.path)) {
+      if (this.isPopupQueuePage) {
         this.pushPagePopupQueueArray({vueName: this.$options.name, toggleName: toggleName});
         this.popupQueueToggleNameArray.push(toggleName);
       } else {
@@ -22,17 +23,19 @@ const popupMixins = {
   computed: {
     ...mapState(['pagePopupQueueArray']),
     popupQueueToggleObject () {
-      let obj = {};
-      this.popupQueueToggleNameArray.forEach((item) => {
-        obj[item] = this[item];
-      });
-      return obj;
+      if (this.isPopupQueuePage) {
+        let obj = {};
+        this.popupQueueToggleNameArray.forEach((item) => {
+          obj[item] = this[item];
+        });
+        return obj;
+      }
     }
   },
   watch: {
     pagePopupQueueArray: {
       handler (val) {
-        if (val.length) {
+        if (this.isPopupQueuePage && val.length) {
           let first = val[0];
           if (first.vueName === this.$options.name) {
             !this[first.toggleName] && (this[first.toggleName] = true);
@@ -43,11 +46,13 @@ const popupMixins = {
     },
     popupQueueToggleObject: {
       handler (val, oldVal) {
-        _.forEach(val, (value, key) => {
-          if (!value && oldVal[key]) {
-            this.shiftPagePopupQueueArray();
-          }
-        });
+        if (this.isPopupQueuePage) {
+          _.forEach(val, (value, key) => {
+            if (!value && oldVal[key]) {
+              this.shiftPagePopupQueueArray();
+            }
+          });
+        }
       },
       deep: true
     }

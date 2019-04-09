b6c2a2fc9fbbe73882f2a224ffb52d697f5166e0
PNFeather <921192114@qq.com>
2019-04-09 11:08:01 +0800

    js队列

---
+++ src/tools/queue/queue.js
@@ -0,0 +1,33 @@
+function Queue () { // 队列
+  let items = [];
+  // 列尾添加
+  this.enqueue = function (element) {
+    if (Array.isArray(element)) {
+      items.push([...element]);
+    } else {
+      items.push(element);
+    }
+  };
+  // 列头删除
+  this.dequeue = function () {
+    return items.shift();
+  };
+  // 获取列头
+  this.front = function () {
+    return items[0];
+  };
+  this.isEmpty = function () {
+    return items.length === 0;
+  };
+  this.size = function () {
+    return items.length;
+  };
+  this.print = function () {
+    console.log(items.toString());
+  };
+  this.clear = function () {
+    items = [];
+  };
+};
+
+export {Queue};
